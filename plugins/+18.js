dfcd4817745ef08db80c212660a559d2b0fa94a5
SkyzobyR <ramahrama806@gmail.com>
2023-01-14 16:43:09 +0800

    R-NXZ

---
+++ plugins/+18.js
@@ -0,0 +1,28 @@
+let handler  = async (m, { conn }) => {
+	
+	await m.reply('[❗] 𝙇𝙤𝙖𝙙𝙞𝙣𝙜...`')
+  conn.reply(m.chat,`${pickRandom(global.bokep)}`, m)
+}
+handler.help = ['filebokep']
+handler.tags = ['hentai']
+handler.command = /^(filebokep)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = true
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+export default handler 
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+global.bokep = []
+//KALAU MAU TAMBAHIN JUGA BISA
---
+++ plugins/_ftroli.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+ let handler  = async (m, { conn, usedPrefix: _p }) => {
+const {
+    MessageType,
+    Mimetype
+} = (await import("@adiwajshing/baileys")).default;
+           
+           const ftrol = {
+  key : {
+                          participant : '0@s.whatsapp.net'
+                        },
+       message: {
+                    orderMessage: {
+                            itemCount : 999,
+                            status: 1,
+                            surface : 1,
+                            message: `𝐑-𝐍𝐗𝙕\n5 Menit`, //Kasih namalu
+                            orderTitle: `𝐑-𝐍𝐗𝙕\n5 Menit`,
+                            thumbnail: await (await fetch(giflogo2)).buffer(), //Gambarnye
+                            sellerJid: '0@s.whatsapp.net' 
+                          }
+                        }
+                      }
+          //m.reply(teks)
+          let jawab = `*Bot aktif selama*\n5 Menit\n\n_©𝐑-𝐍𝐗𝙕_`.trim()
+conn.sendMessage(m.chat,{text:wm}, {quoted: ftrol})
+}
+
+
+handler.help = ['troli']
+
+handler.tags = ['ᴘᴇɴᴅɪɴɢ sᴛᴜғғ']
+
+handler.command = /^troli$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+
+export default handler
